96151420c722bc4acca2eb08863575d1122bb04f
OOLebedenko <oolebedenko@gmail.com>
2026-02-27 17:52:35 +0300

    add template hello by name and surname

---
+++ template2.py
@@ -0,0 +1,9 @@
+def print_hello(name: str, surname: str) -> None:
+    """
+    print hello!
+
+    :param name: name of man
+    :param surname: surname of man
+    :return:
+    """
+    # enter your code here
